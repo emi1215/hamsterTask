31930fb5b66425601d8edbfb9b454604c5f8b2c9
emi <bluming_blue@icloud.com>
2022-03-17 05:27:28 +0900

    runした時に、画像が正しく表示されません。

---
+++ hamsterTask/TaskMakeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskMakeViewController.swift
+//  hamsterTask
+//
+//  Created by 横田瑛美 on 2022/03/17.
+//
+
+import UIKit
+
+class TaskMakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
